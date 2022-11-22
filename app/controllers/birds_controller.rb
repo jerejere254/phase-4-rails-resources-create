71f259e296dfa36d6bd8c392c7d4435238d8b014
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-22 11:58:31 +0300

    add

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,11 +1,9 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -14,6 +12,11 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
   end
 
 end
